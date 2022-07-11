b2a5405852b28c16a25d3cb79eddf26d09be1b86
Fahad Hossain <set.name.fahad@gmail.com>
2022-07-11 16:15:48 +0200

    feat: update deployment config for express

--- app/content/markdown.server.ts
+++ app/content/markdown.server.ts
@@ -1,29 +1,29 @@
 import { sanitize } from './markdown.config.server'
-const unified = import('unified').then(m => m.unified)
-const remarkGfm = import('remark-gfm').then(m => m.default)
-const rehypeRaw = import('rehype-raw').then(m => m.default)
-const remarkMath = import('remark-math').then(m => m.default)
-const remarkParse = import('remark-parse').then(m => m.default)
-const stripMarkdown = import('strip-markdown').then(m => m.default)
-const remarkStringify = import('remark-stringify').then(m => m.default)
-const remarkRehype = import('remark-rehype').then(m => m.default)
-const rehypeSanitize = import('rehype-sanitize').then(m => m.default)
-const rehypeStringify = import('rehype-stringify').then(m => m.default)
-const rehypeKatex = import('rehype-katex').then(m => m.default)
-const rehypePrism = import('rehype-prism-plus').then(m => m.default)
+import { unified } from 'unified'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import remarkMath from 'remark-math'
+import remarkParse from 'remark-parse'
+import stripMarkdown from 'strip-markdown'
+import remarkStringify from 'remark-stringify'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+import rehypeKatex from 'rehype-katex'
+import rehypePrism from 'rehype-prism-plus'
 
 export default async function renderMarkdown(markdownText: string) {
   const uni = await unified
   const result = await uni()
-    .use(await remarkParse)
-    .use(await remarkGfm)
-    .use(await remarkMath)
-    .use(await remarkRehype, { allowDangerousHtml: true })
-    .use(await rehypeKatex, { displayMode: false })
-    .use(await rehypeRaw)
-    .use(await rehypePrism, { showLineNumbers: true, ignoreMissing: true })
-    .use(await rehypeSanitize, await sanitize())
-    .use(await rehypeStringify)
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkMath)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeKatex, { displayMode: false })
+    .use(rehypeRaw)
+    .use(rehypePrism, { showLineNumbers: true, ignoreMissing: true })
+    .use(rehypeSanitize, await sanitize())
+    .use(rehypeStringify)
     .process(markdownText)
 
   return result.toString()
@@ -33,12 +33,11 @@ export async function renderExcerpt(
   markdownText: string,
   excerptLength: number
 ) {
-  const uni = await unified
-  const result = await uni()
-    .use(await remarkParse)
-    .use(await remarkGfm)
-    .use(await stripMarkdown)
-    .use(await remarkStringify)
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(stripMarkdown)
+    .use(remarkStringify)
     .process(markdownText)
   const contentText = result.toString().trim().replace(/\s+/g, ' ')
   const excerpt = contentText.slice(0, excerptLength)
